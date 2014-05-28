c90c007fc8912394ac357d23ec9942c9d9ac9103
Henrik Joreteg <hjoreteg@gmail.com>
2014-05-27 23:19:41 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# hapi-dummy-api
+
+Generate dummy APIs for building clientside apps before the real API is done.
+
+Don't wait for the real thing to be done before you get started building the client app.
+
+This util lets you generate little dummy APIs that can be registered as hapi plugins for a hapi.js server.
+
+
+## install
+
+```
+npm install hapi-dummy-api
+```
+
+## example
+
+```javascript
+var Hapi = require('hapi');
+var config = require('getconfig');
+var server = new Hapi.Server('localhost', config.http.port);
+
+// require our dummy API generator
+var API = require('hapi-dummy-api');
+
+
+// all these config items are optionals
+var tempAPI = new API({
+    // Optionally give it some starting data (should be an array)
+    // defaults to [];
+    data: [
+        {
+            id: 0,
+            name: 'mary'
+        },
+        {
+            id: 1,
+            name: 'bob'
+        }
+    ],
+    // the root RESTful resource URL
+    rootUrl: '/api/people',
+    // specify which property name should be the "id"
+    // defaults to "id"
+    idProperty: 'id',
+    // Optionally give it a delay (in milliseconds) to simulate network latency
+    // as you'd have in real clientapp situation.
+    delay: 200,
+    // hapi plugin name, defaults to 'api'
+    name: 'fake-people-api',
+    // hapi plugin version, defaults to '0.0.0',
+    version: '0.0.1'
+});
+
+
+server.pack.register(fakeApi, function (err) {
+    if (err) throw err;
+    // If everything loaded correctly, start the server:
+    server.start(function (err) {
+        if (err) throw err;
+        console.log("running at: http://localhost:" + config.http.port + ");
+    });
+});
+```
+
+## additional info
+
+There's really not much more to it, the source is < 100 lines and should be quite readable. 
+
+This is meant for development only, obviously. It would be the worlds most terrible and insecure production API. Please don't use it for that. Use it to aid development of client apps.
+
+
+## credits
+
+If you like this follow [@HenrikJoreteg](http://twitter.com/henrikjoreteg) on twitter.
+
+## license
+
+MIT
+
---
+++ hapi-dummy-api.js
@@ -0,0 +1,90 @@
+// Instantiate this, then register it as a hapi plugin
+// and you've got yourself a fake API for quick/easy
+// clientside dev.
+var _ = require('underscore');
+
+
+function API(spec) {
+    this.data = spec.data || [];
+    this.root = spec.rootUrl || '/api';
+    this.idProperty = spec.idProperty || 'id';
+    this.delay = spec.delay || 0;
+    this.name = spec.name || 'api';
+    this.version = spec.version || '0.0.0';
+
+    this.idCounter = this.data.length;
+
+    // bind our register function to maintain scope
+    // since hapi doesn't do that for us
+    this.register = _.bind(this.register, this);
+}
+
+API.prototype.get = function (id) {
+    return _.findWhere(this.data, {id: parseInt(id + '', 10)});
+};
+
+API.prototype.register = function (plugin, options, next) {
+    var self = this;
+
+    plugin.route({
+        method: 'GET',
+        path: self.root,
+        handler: function (request, reply) {
+            _.delay(function () {
+                reply(self.data);
+            }, self.delay);
+        }
+    });
+
+    plugin.route({
+        method: 'POST',
+        path: self.root,
+        handler: function (request, reply) {
+            var person = request.payload;
+            person.id = self.idCounter++;
+            self.data.push(person);
+            _.delay(function () {
+                reply(person).code(201);
+            }, self.delay);
+        }
+    });
+
+    plugin.route({
+        method: 'GET',
+        path: self.root + '/{' + self.idProperty + '}',
+        handler: function (request, reply) {
+            var found = self.get(request.params[self.idProperty]);
+            _.delay(function () {
+                reply(found).code(found ? 200 : 404);
+            }, self.delay);
+        }
+    });
+
+    plugin.route({
+        method: 'DELETE',
+        path: self.root + '/{' + self.idProperty + '}',
+        handler: function (request, reply) {
+            var found = self.get(request.params[self.idProperty]);
+            if (found) self.data = _.without(self.data, found);
+            _.delay(function () {
+                reply(found).code(found ? 200 : 404);
+            }, self.delay);
+        }
+    });
+
+    plugin.route({
+        method: 'PUT',
+        path: self.root + '/{' + self.idProperty + '}',
+        handler: function (request, reply) {
+            var found = self.get(request.params[self.idProperty]);
+            if (found) _.extend(found, request.payload);
+            _.delay(function () {
+                reply(found).code(found ? 200 : 404);
+            }, self.delay);
+        }
+    });
+
+    next();
+};
+
+module.exports = API;
